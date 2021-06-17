b628729b132bdba24a39eba248deb58d59f3f905
bessyrey <bessy.reyes@unah.hn>
2021-06-16 20:06:28 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ function App() {
   return (
    <div className="container mt-5">
      <h1>Tarea</h1>
-     <hr/>:
+     <hr/>
+     <div className= "row">
+      <div className="col-8">
+       <h4 className="text-center">Lista de Tareas</h4>
+       <ul className="list-group">
+        <li className="list-group-item">
+          <span className="lead">Nombre de la Tarea</span>
+          <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+          <button className="btn btn-warning btn-sm float-right">Editar</button>
+        </li>
+        </ul>
+      </div>
+      <div className="col-4">
+       <h4 className="text-center">Formulario</h4>
+       <form>
+         <input
+         type="text"
+         className="form-control mb-2"
+         placeholder="Ingrese la Tarea..."
+         />
+         <button className="btn btn-dark btn-block" type="submit">
+         Agregar
+         </button>
+       </form>
+      </div>
+
+     </div>
    </div>
   )
 }
