6ce0eeed083da503a4dbc68b7bee768c8b86b130
bessyrey <bessy.reyes@unah.hn>
2021-06-18 00:35:07 -0600

    add task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import { isEmpty } from 'lodash'
+import React, { useState } from 'react'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  
+  const addTask = (e) => {
+    e.preventDefault()
+    if (isEmpty(task)) {
+      console.log("Task empty")
+      return
+    }
+
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([...tasks, newTask ])
+    setTask("")
+  }
+  
+
   return (
    <div className="container mt-5">
      <h1>Tarea</h1>
@@ -9,27 +31,35 @@ function App() {
       <div className="col-8">
        <h4 className="text-center">Lista de Tareas</h4>
        <ul className="list-group">
-        <li className="list-group-item">
-          <span className="lead">Nombre de la Tarea</span>
-          <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-          <button className="btn btn-warning btn-sm float-right">Editar</button>
-        </li>
+        {
+          tasks.map((task) => (
+            <li className="list-group-item" key={task.div}>
+           <span className="lead">{task.name}</span>
+           <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+           <button className="btn btn-warning btn-sm float-right">Editar</button>
+           </li>
+
+          ))    
+        }
         </ul>
       </div>
       <div className="col-4">
        <h4 className="text-center">Formulario</h4>
-       <form>
+       <form onSubmit={addTask}>
          <input
-         type="text"
-         className="form-control mb-2"
-         placeholder="Ingrese la Tarea..."
+          type="text"
+          className="form-control mb-2"
+          placeholder="Ingrese la Tarea..." 
+          onChange={(text) => setTask(text.target.value) }
+          value={task}
          />
-         <button className="btn btn-dark btn-block" type="submit">
+         <button className="btn btn-dark btn-block"
+          type="submit"
+          >
          Agregar
          </button>
        </form>
       </div>
-
      </div>
    </div>
   )
