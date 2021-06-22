273df81dbfeb1d5b48531482b491bd57891895c7
bessyrey <bessy.reyes@unah.hn>
2021-06-21 19:38:21 -0600

    add task

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import shortid from 'shortid'                 /*se agrega la libreria shortied *
 function App() {
   const [task, setTask] = useState("")       /*estado que almacena el nombre de la tarea nueva*/
   const [tasks, setTasks] = useState([])     /*se crea el arreglo para las nuevas tareas*/
+  const [editMode, setEditMode]= useState(false)
+  const [id, setId] = useState("")            /*Guarda el estado del id de la tarea a modificar */
   
   const addTask = (e) => {
     e.preventDefault()                       /*creamos un evento*/
@@ -22,11 +24,32 @@ function App() {
     setTask("")
   }
 
+  const saveTask = (e) => {
+    e.preventDefault()
+    if(isEmpty(task)){
+      console.log("Task empty")
+      return 
+    }
+ 
+     const editedTasks = tasks.map(item => item.id === id ? { id, name: task}: item)
+     setTasks(editedTasks)
+     setEditMode(false)
+     setTask("")
+     setId("")
+  }
+
    const deleteTask = (id) => {        /*se filtran todas las tareas menos la q el ususrio borro*/
      const filteredTasks = tasks.filter(task => task.id !== id)
      setTasks(filteredTasks)
    }
 
+   const editTask = (theTask) => {        /*se filtran todas las tareas menos la q el ususrio borro*/
+    setTask(theTask.name)
+    setEditMode(true)
+    setId(theTask.id)
+  } 
+   
+   
   return (
    <div className="container mt-5">
      <h1>Tarea</h1>
@@ -36,7 +59,7 @@ function App() {
        <h4 className="text-center">Lista de Tareas</h4>
        
        {
-         size(tasks) == 0 ? (
+         size(tasks) === 0 ? (
            <h5 className="text-center">Aun no hay Tareas Programadas.</h5>
          ) : ( 
             <ul className="list-group">
@@ -46,14 +69,15 @@ function App() {
                   <span className="lead">{task.name}</span>
                   <button 
                     className="btn btn-danger btn-sm float-right mx-2"
-                   onClick = {() => deleteTask(task.id)}           
+                    onClick = {() => deleteTask(task.id)}           
                   >
                   Eliminar
                   </button>
                   <button 
                      className="btn btn-warning btn-sm float-right"
+                     onClick={() => editTask(task)}
                      >
-                     Editar
+                     { editMode ? "Guardar" : "Editar" }
                  </button>
                </li>
 
@@ -65,8 +89,10 @@ function App() {
 
       </div>
       <div className="col-4">
-       <h4 className="text-center">Formulario</h4>
-       <form onSubmit={addTask}>
+       <h4 className="text-center">
+       { editMode ? "Modificar Tarea": "Agregar Tarea"}
+       </h4>
+       <form onSubmit={editMode ? saveTask : addTask}>
          <input
           type="text"
           className="form-control mb-2"
@@ -74,10 +100,10 @@ function App() {
           onChange={(text) => setTask(text.target.value) }  /*para cambiar el estado */
           value={task}
          />
-         <button className="btn btn-dark btn-block"
+         <button className={ editMode ? "btn btn-warning btn-block" : "btn btn-dark btn-block"}
           type="submit"
           >
-         Agregar
+          { editMode ? "Guardar" : "Agregar" }
          </button>
        </form>
       </div>
